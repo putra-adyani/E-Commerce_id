3fa83619e9c13d7008994ae019dd54fe7de4e555
Dimas Putra Adyani <18106050036@student.uin-suka.ac.id>
2021-07-18 19:43:27 +0700

    add header, footer, and login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,22 @@
 @extends('master')
 @section('content')
-<h1>Login Page</h1>
+<div class="container custom-login">
+    <div class="row mt-3">
+        <div class="col-sm-4 offset-4">
+            <form>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-form-label">Email Address</label>
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="email">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="password">
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Sign in</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+@endsection 
