76f085ae7b2dc1254cbb7cb5300c8a70e548121d
Dimas Putra Adyani <18106050036@student.uin-suka.ac.id>
2021-07-19 02:08:58 +0700

    create logout

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,20 +1,23 @@
 <?php
 use App\Http\Controllers\ProductController;
-$total = ProductController::cartItem();
+$total = 0;
+if (Session::has('user')) {
+    $total = ProductController::cartItem();
+}
 
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
     
-    <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">E-Comm</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" href="#">Orders</a>
@@ -28,6 +31,11 @@ $total = ProductController::cartItem();
             </form>
             <ul class="nav navbar-nav">
                 <li><a href="#" class="nav-link">cart({{ $total }})</a></li>
+                @if (Session::has('user'))
+                    <li><a href="/logout" class="nav-link">Logout</a></li>
+                @else
+                    <li><a href="/login" class="nav-link">Login</a></li>
+                @endif
             </ul>
         </div>
     </div>
